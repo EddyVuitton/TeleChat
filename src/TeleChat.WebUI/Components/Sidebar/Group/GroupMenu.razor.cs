ab2c0fa69d5ca4c4832478c7c87898dc1ad659f3
Edward Gadawski <EdwardGadawski@gmail.com>
2024-10-05 21:16:50 +0200

    Zmiana w wygladzie GroupMenu / Dodanie customowego popovera

--- src/TeleChat.WebUI/Components/Sidebar/Group/GroupMenu.razor.cs
+++ src/TeleChat.WebUI/Components/Sidebar/Group/GroupMenu.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using TeleChat.WebUI.Customs;
 using TeleChat.WebUI.Services.Hub;
 
 namespace TeleChat.WebUI.Components.Sidebar.Group;
@@ -12,16 +13,11 @@ public partial class GroupMenu
 
     #endregion
 
-    #region Fields
-
-    private bool _isOpenPopoverMenu;
-    private string _popoverStyle = string.Empty;
-
-    #endregion
-
     #region Properties
 
     [Parameter] public GroupItem? GroupItem { get; init; }
+
+    private TCPopover? _popoverRef { get; set; }
 
     #endregion
 
@@ -29,41 +25,12 @@ public partial class GroupMenu
 
     public void TogglePopoverMenu(MouseEventArgs args)
     {
-        if (_isOpenPopoverMenu)
-        {
-            ClosePopoverMenu();
-        }
-        else
-        {
-            OpenPopoverMenu(args);
-        }
+        _popoverRef?.TogglePopoverMenu(args);
     }
 
     #endregion
 
     #region PrivateMethods
-
-    private void ClosePopoverMenu()
-    {
-        _isOpenPopoverMenu = false;
-        _popoverStyle = string.Empty;
-        StateHasChanged();
-    }
-
-    private void OpenPopoverMenu(MouseEventArgs args)
-    {
-        SetPopoverStyle(args);
-        _isOpenPopoverMenu = true;
-
-        StateHasChanged();
-    }
-
-    private void SetPopoverStyle(MouseEventArgs args)
-    {
-        var clientX = args?.ClientX.ToString("0.##");
-        var clientY = args?.ClientY.ToString("0.##");
-        _popoverStyle = $"padding: 10px 0px; position: fixed !important; left: {clientX}px; top: {clientY}px;";
-    }
 
     private async Task DeleteGroupChat()
     {
